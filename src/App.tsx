511a6e21a6f046df2897400e1825727f4be06987
unknown <nguyenvotuanthanh0307@gmail.com>
2023-03-15 11:26:48 +0700

    Testing cicd

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Header from './components/Header/header';
 import Home from './components/Home/home';
 
 function App() {
+  console.log('Testing CICD');
+
   return (
     <>
       <div>Testing</div>
